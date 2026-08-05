242d53cf250ac023aaf2a5918c94672bf6b46b70
欧阳俊杰 <wangjunjie0150@qq.com>
2026-08-05 17:17:10 +0800

    更新 versions.js

--- versions.js
+++ versions.js
@@ -95,7 +95,7 @@ const versionConfig = {
             date: '2026-07',
             description: '有奖竞猜活动开始啦，欢迎参与！集中修复了许多bug，提升使用体验。',
             filename: '小柠同学2.5.0.apk'
-        }，
+        },
         {
             version: 'v2.6.0',
             date: '2026-08-04',
